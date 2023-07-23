d037bd79668b0010315516d0b0fdc2231e100b00
zerzif-chouaib-git <zerzif.bouchaib@gmail.com>
2023-07-23 22:53:42 +0100

    inter process communication + service discovery using netflix eureka

--- microservices-programming-techie/order-service/src/main/java/com/programmingtechie/orderservice/config/WebClientConfig.java
+++ microservices-programming-techie/order-service/src/main/java/com/programmingtechie/orderservice/config/WebClientConfig.java
@@ -1,2 +1,16 @@
-package com.programmingtechie.orderservice.config;public class WebClientConfig {
+package com.programmingtechie.orderservice.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webCLientBuilder(){
+        return WebClient.builder();
+    }
 }
--- microservices-programming-techie/order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ microservices-programming-techie/order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.programmingtechie.orderservice.service;
 
+import com.programmingtechie.orderservice.dto.InventoryResponse;
 import com.programmingtechie.orderservice.dto.OrderLineItemsDto;
 import com.programmingtechie.orderservice.dto.OrderRequest;
 import com.programmingtechie.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.programmingtechie.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,6 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -27,7 +31,25 @@ public class OrderService {
                 .map(this::mapToDto)
                 .toList();
         order.setOrderLineItems(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItems().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+        if (Boolean.TRUE.equals(allProductsInStock)) {
+            orderRepository.save(order);
+        }else{
+            throw new IllegalArgumentException("Product is not in stock please try again later");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
